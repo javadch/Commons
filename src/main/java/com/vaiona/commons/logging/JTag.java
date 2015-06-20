72211ae686f0fc5e34017c10b244ac0ff8601a3b
javadch <chamanara@gmail.com>
2015-06-21 00:07:47 +0200

    Added debugging points, like jtags

---
+++ src/main/java/com/vaiona/commons/logging/JTag.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vaiona.commons.logging;
+
+/**
+ *
+ * @author Javad Chamanara <chamanara@gmail.com>
+ */
+public class JTag {
+    // It is just a jtag style proble function that when called, brings the object data here
+    // to be observed. it is mainly used in the dynamically created classes.
+    public static void probe(Object object){
+       
+    } 
+}
